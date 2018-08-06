ff226cf4966a78f433c8327069eecf7f569c3f9d
Ondrej Rysavy <rysavy@fit.vutbr.cz>
2018-08-06 07:38:55 +0200

    some ui defs

--- tarzan-ui/dashboard/ClientApp/components/services/services.ts
+++ tarzan-ui/dashboard/ClientApp/components/services/services.ts
@@ -3,7 +3,38 @@ import { Component } from 'vue-property-decorator';
 
 @Component
 export default class ServicesComponent extends Vue {
-    loading: boolean = true;
+    loading: boolean = false;
+    dataSource = [{
+        protocol: 'HTTP',
+        flows: 13256,
+        packets: 1242323,
+        octets: 242353456,
+        minPackets: 2, 
+        minOctets: 132,
+        maxPackets: 3424,
+        maxOctets: 122456,
+        avgPackets: 24,
+        avgOctets: 35346,
+        minDuration: 400,
+        maxDuration: 123425,
+        avgDuration: 3453
+    },
+    {
+        protocol: 'DNS',
+        flows: 13256,
+        packets: 1242323,
+        octets: 242353456,
+        minPackets: 2,
+        minOctets: 132,
+        maxPackets: 3424,
+        maxOctets: 122456,
+        avgPackets: 24,
+        avgOctets: 35346,
+        minDuration: 400,
+        maxDuration: 123425,
+        avgDuration: 3453
+    }
+    ];
 }
 
 
