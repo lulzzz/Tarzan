fbee7ecc5db2774e4ecc1a51d8e212daf7dee902
Ondrej Rysavy <rysavy@fit.vutbr.cz>
2018-10-15 20:47:40 +0200

    pcaploader shows progress

--- tarzan-ingest/packets/Dotnet/Tarzan.Nfx.IgniteServer/Program.cs
+++ tarzan-ingest/packets/Dotnet/Tarzan.Nfx.IgniteServer/Program.cs
@@ -1,27 +1,81 @@
 ﻿using Apache.Ignite.Core;
+using Apache.Ignite.Core.Cache.Configuration;
+using Apache.Ignite.Core.Configuration;
 using Apache.Ignite.Core.Deployment;
+using Apache.Ignite.Core.Discovery.Tcp;
+using Apache.Ignite.Core.Discovery.Tcp.Static;
+using Microsoft.Extensions.CommandLineUtils;
 using System;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace Tarzan.Nfx.IgniteServer
 {
     class Program
     {
+        const int DefaultOffHeapMemory = 1024;
+        const int DefaultThreads = 8;
         static void Main(string[] args)
         {
-            Console.WriteLine("Tarzan.Nfx.IgniteServer");
-            var config = new IgniteConfiguration
+            var commandLineApplication = new CommandLineApplication(true);
+
+            var offheapArgument = commandLineApplication.Option("-m|--offheap", "Size of offheap memory given in megabytes.", CommandOptionType.SingleValue);
+            var threadsArgument = commandLineApplication.Option("-t|--threads", "Size of a thread pool used by the server.", CommandOptionType.SingleValue);
+
+            
+            commandLineApplication.OnExecute(() =>
             {
-                PeerAssemblyLoadingMode = PeerAssemblyLoadingMode.CurrentAppDomain
-            };
-            using (var ignite = Ignition.Start(config))
+                var maxThreads = threadsArgument.HasValue() ? Math.Min(Environment.ProcessorCount, Int32.TryParse(threadsArgument.Value(), out var requiredCores) ? requiredCores : DefaultThreads) : DefaultThreads;
+                var maxOffHeapMemory = offheapArgument.HasValue() ? Int32.Parse(offheapArgument.Value()) : DefaultOffHeapMemory;
+
+                Console.WriteLine($"Tarzan.Nfx.IgniteServer [maxThreads={maxThreads}, maxOffHeap={maxOffHeapMemory}MB]");
+                var cfg = new IgniteConfiguration
+                {
+                    PublicThreadPoolSize = maxThreads,
+                    DataStorageConfiguration = new DataStorageConfiguration
+                    {
+                        DefaultDataRegionConfiguration = new DataRegionConfiguration
+                        {
+                            Name = "defaultRegion",
+                            PersistenceEnabled = true,
+                        },
+                    },
+                    PeerAssemblyLoadingMode = PeerAssemblyLoadingMode.CurrentAppDomain,
+                    DiscoverySpi = new Apache.Ignite.Core.Discovery.Tcp.TcpDiscoverySpi
+                    {
+                        IpFinder = new TcpDiscoveryStaticIpFinder
+                        {
+                            Endpoints = new[] { "127.0.0.1:47500..47501" }
+                        },
+                    }
+
+
+                };
+                using (var ignite = Ignition.Start(cfg))
+                {
+                    var mem = ignite.GetDataRegionMetrics();
+                    var cts = new CancellationTokenSource();
+                    ignite.Stopped += (s, e) => cts.Cancel();
+                    var localSpidPort = (ignite.GetConfiguration().DiscoverySpi as TcpDiscoverySpi).LocalPort;
+                    Console.WriteLine($"Ignite server is running (Local SpiDiscovery Port={localSpidPort}), press CTRL+C to terminate.");
+                    ignite.GetCluster().SetActive(true);
+                    cts.Token.WaitHandle.WaitOne();
+                    Console.WriteLine("Ignite Server Gracefully Shutdowned.");
+                }
+                return 0;
+            });
+
+
+            try
             {
-                var cts = new CancellationTokenSource();
-                ignite.Stopped += (s, e) => cts.Cancel();
-                Console.WriteLine("Ignite server is running, press CTRL+C to terminate.");
-                cts.Token.WaitHandle.WaitOne();
-                Console.WriteLine("Ignite Server Gracefully Shutdowned.");
+                commandLineApplication.Execute(args);
+            }
+            catch (CommandParsingException e)
+            {
+                commandLineApplication.Error.WriteLine($"ERROR: {e.Message}");
+            }
+            catch (ArgumentException e)
+            {
+                commandLineApplication.Error.WriteLine($"ERROR: {e.Message}");
             }
         }
     }
--- tarzan-ingest/packets/Dotnet/Tarzan.Nfx.PcapLoader/Program.cs
+++ tarzan-ingest/packets/Dotnet/Tarzan.Nfx.PcapLoader/Program.cs
@@ -3,7 +3,9 @@ using Apache.Ignite.Core.Client;
 using Apache.Ignite.Core.Client.Cache;
 using Apache.Ignite.Core.Communication.Tcp;
 using Microsoft.Extensions.CommandLineUtils;
+using Microsoft.Extensions.Logging.Console;
 using SharpPcap;
+using ShellProgressBar;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -31,7 +33,7 @@ namespace Tarzan.Nfx.PcapLoader
                 if (clusterArgument.HasValue() &&
                 (fileArgument.HasValue() || folderArgument.HasValue()))
                 {
-                    ExecuteLoader(clusterArgument.Values, fileArgument.Values, folderArgument.Values);
+                    new Program().ExecuteLoader(clusterArgument.Values, fileArgument.Values, folderArgument.Values);
                     return 0;
                 }
                 else
@@ -55,7 +57,9 @@ namespace Tarzan.Nfx.PcapLoader
             }
         }
 
-        private static void ExecuteLoader(List<string> clusterEp, List<string> files, List<string> folders)
+        ConsoleLogger m_logger = new ConsoleLogger("Loader", (s, ll) => true, true);
+
+        private void ExecuteLoader(List<string> clusterEp, List<string> files, List<string> folders)
         {
             var ep = IPEndPointExtensions.Parse(clusterEp.First(), 10800);
             var cfg = new IgniteClientConfiguration
@@ -77,55 +81,88 @@ namespace Tarzan.Nfx.PcapLoader
             }
 
 
+
+            var pbRootOptions = new ProgressBarOptions
+            {
+                ForegroundColor = ConsoleColor.Yellow,
+                BackgroundColor = ConsoleColor.DarkYellow,
+                ProgressCharacter = '─',
+               
+            };
+            var pbChildOptions = new ProgressBarOptions
+            {
+                ForegroundColor = ConsoleColor.Green,
+                BackgroundColor = ConsoleColor.DarkGreen,
+                ProgressCharacter = '─',
+                ProgressBarOnBottom = true
+            };
+
             var flowKeyProvider = new FrameKeyProvider();
             const int CACHE_SIZE = 1000;
             using (var client = Ignition.StartClient(cfg))
             {
-                foreach (var fileInfo in pcpafiles)
+                using (var pbar = new ProgressBar(pcpafiles.Count, "", pbRootOptions))
                 {
-                    Console.WriteLine($"Processing file '{fileInfo.Name}', total size {fileInfo.Length}.");
-                    var sw = new Stopwatch();
-
-                    using (var device = new FastPcapFileReaderDevice(fileInfo.FullName))
+                    foreach (var fileInfo in pcpafiles)
                     {
-                        var packetCache = client.GetOrCreateCache<int, Frame>(fileInfo.FullName);
-                        Console.WriteLine($"Packets already in cache {packetCache.GetSize()}.");
-                        sw.Start();
-                        device.Open();
-                        
-                        int frameIndex = 0;
-                        var frameArray = new KeyValuePair<int, Frame>[CACHE_SIZE];
-                        var putCapturesToCacheTask = Task.CompletedTask;
+                        pbar.Message = $"Processing file '{fileInfo.Name}', total size {fileInfo.Length} bytes. ";
+                        var sw = new Stopwatch();
 
-                        RawCapture rawCapture = null;
-                        while ((rawCapture = device.GetNextPacket()) != null)
+                        using (var device = new FastPcapFileReaderDevice(fileInfo.FullName))
+                        using (var pbStore = pbar.Spawn((int)fileInfo.Length, $"Storing packets in Ignite cache...", pbChildOptions))
+                        using (var pbLoad = pbar.Spawn((int)fileInfo.Length, $"Loading packets from pcap file...", pbChildOptions))
                         {
-                            var frame = new Frame
-                            {
-                                LinkLayer = (LinkLayerType)rawCapture.LinkLayerType,
-                                Timestamp = rawCapture.Timeval.ToUnixTimeMilliseconds(),
-                                Data = rawCapture.Data
-                            };
-                            var key = flowKeyProvider.GetKey(frame);
-                            var frameHashCode = key.HashCode;
+                            var packetCache = client.GetOrCreateCache<int, Frame>(fileInfo.Name);
+                            m_logger.WriteMessage(Microsoft.Extensions.Logging.LogLevel.Information, "PcapLoader", 1, $"Packets already in cache {packetCache.GetSize()}.", null);
 
-                            frameArray[frameIndex % CACHE_SIZE] = KeyValuePair.Create(frameIndex, frame);
-                            if (frameIndex % CACHE_SIZE == CACHE_SIZE-1)
+                            sw.Start();
+                            device.Open();
+
+                            int frameIndex = 0;
+                            int estimatedDataOffset = 0;
+                            var frameArray = new KeyValuePair<int, Frame>[CACHE_SIZE];
+                            var putCapturesToCacheTask = Task.CompletedTask;
+
+                            RawCapture rawCapture = null;
+                            while ((rawCapture = device.GetNextPacket()) != null)
                             {
-                                
-                                var cacheToStore = frameArray;
-                                frameArray = new KeyValuePair<int, Frame>[CACHE_SIZE];
-                                putCapturesToCacheTask = putCapturesToCacheTask.ContinueWith((_) => { Console.Write("s"); return packetCache.PutAllAsync(cacheToStore); });
-                                Console.Write("p");
+                                estimatedDataOffset += rawCapture.Data.Length + 8;
+                                pbLoad.Tick(estimatedDataOffset);
+                                var frame = new Frame
+                                {
+                                    LinkLayer = (LinkLayerType)rawCapture.LinkLayerType,
+                                    Timestamp = rawCapture.Timeval.ToUnixTimeMilliseconds(),
+                                    Data = rawCapture.Data
+                                };
+
+                                var key = flowKeyProvider.GetKey(frame);
+                                var frameHashCode = key.HashCode;
+
+                                frameArray[frameIndex % CACHE_SIZE] = KeyValuePair.Create(frameIndex, frame);
+                                if (frameIndex % CACHE_SIZE == CACHE_SIZE - 1)
+                                {
+
+                                    var cacheToStore = frameArray;
+                                    frameArray = new KeyValuePair<int, Frame>[CACHE_SIZE];
+                                    putCapturesToCacheTask = putCapturesToCacheTask.ContinueWith(async (_) => { await packetCache.PutAllAsync(cacheToStore); pbStore.Tick(estimatedDataOffset); });
+                                }
+                                frameIndex++;
                             }
-                            frameIndex++;
-                        }
-                        putCapturesToCacheTask = putCapturesToCacheTask.ContinueWith((_) => { Console.Write("s"); return packetCache.PutAllAsync(frameArray.Take(frameIndex % CACHE_SIZE)); });
 
-                        putCapturesToCacheTask.Wait();
-                        sw.Stop();
-                        Console.WriteLine($"Read packets: {frameIndex}, packets in cache {packetCache.GetSize()}, total time elapsed: {sw.Elapsed}.");
-                        device.Close();
+                            pbLoad.Tick((int)fileInfo.Length);
+                            putCapturesToCacheTask = putCapturesToCacheTask.ContinueWith(async (_) => { await packetCache.PutAllAsync(frameArray.Take(frameIndex % CACHE_SIZE)); pbStore.Tick(estimatedDataOffset); });
+
+                            // wait till we store all data to cache:
+                            putCapturesToCacheTask.Wait();
+
+                            pbStore.Tick((int)fileInfo.Length);
+                            pbar.Tick();
+                            sw.Stop();
+
+                            m_logger.WriteMessage(Microsoft.Extensions.Logging.LogLevel.Information, "PcapLoader", 1, $"Read packets: {frameIndex}, packets in cache {packetCache.GetSize()}, total time elapsed: {sw.Elapsed}.", null);
+
+                            device.Close();
+                        }
                     }
                 }
             }
