77baad04fac2bd547a8875bbcc74a0bb8c7b1eed
Ondrej Rysavy <rysavy@fit.vutbr.cz>
2018-10-23 21:28:27 +0200

    solution cleaning

--- out/server/run-cluster.cmd
+++
@@ -1,7 +0,0 @@
-﻿
-
-START dotnet Tarzan.Nfx.IgniteServer.dll 
-START dotnet Tarzan.Nfx.IgniteServer.dll 
-START dotnet Tarzan.Nfx.IgniteServer.dll 
-START dotnet Tarzan.Nfx.IgniteServer.dll 
-C:\Apache\Ignite\bin\control --baseline
