483188c087112f142bc8e56bc1bb26245f680cc9
Ondrej Rysavy <rysavy@fit.vutbr.cz>
2018-10-23 21:06:13 +0200

    Rename FlowTracker -> Analyzers

--- tarzan-ingest/packets/Dotnet/Tarzan.Nfx.Ingest/bin/Debug/netcoreapp2.0/run_ignite_cluster.cmd
+++
@@ -1,4 +0,0 @@
-@ECHO Running %1 instances of Ignite Server
-@FOR /L %%G IN (1,1,%1) DO START dotnet Tarzan.Nfx.Ingest.dll start-ignite
-
-REM START dotnet Tarzan.Nfx.Ingest.dll track-flows -folder D:\Captures\ids\testbed-12jun-128 -cassandra "localhost" -namespace testbed -create
