844236028590cbd48f2936dbfe9ae1f3d5fc0f38
Ondrej Rysavy <rysavy.ondrej@gmail.com>
2018-11-03 15:15:19 +0100

    add post build action

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
+# build all projects using dotnet:
 dotnet --list-sdks
-dotnet restore Tarzan.sln 
 dotnet build Tarzan.sln
+
+# copy output from projects to artifacts folder:
+cp -a src/Tarzan.Nfx.Analyzers/bin/Debug/netcoreapp2.0/* artifacts
+cp -a src/Tarzan.Nfx.IgniteServer/bin/Debug/netcoreapp2.0/* artifacts
+cp -a src/Tarzan.Nfx.PcapLoader/bin/Debug/netcoreapp2.0/* artifacts
+
