48981d81668333e5f11e67b956fb1c3103e823b6
Ondrej Rysavy <rysavy.ondrej@gmail.com>
2018-11-03 14:44:40 +0100

    add scripts

--- src/Tarzan.Nfx.IgniteServer/run-inmem.cmd
+++
@@ -1,6 +0,0 @@
-﻿REM
-START dotnet Tarzan.Nfx.IgniteServer.dll  --onheap 1024 --offheap 2048
-START dotnet Tarzan.Nfx.IgniteServer.dll  --onheap 1024 --offheap 2048
-START dotnet Tarzan.Nfx.IgniteServer.dll  --onheap 1024 --offheap 2048
-START dotnet Tarzan.Nfx.IgniteServer.dll  --onheap 1024 --offheap 2048
-
--- src/Tarzan.Nfx.PcapLoader/load.cmd
+++
@@ -1,2 +0,0 @@
-﻿rem 
-dotnet Tarzan.Nfx.PcapLoader.dll -Cluster 127.0.0.1:47500 -SourceFile d:\Captures\ids\testbed-12jun-128\testbed-12jun-000.pcap -Mode stream
