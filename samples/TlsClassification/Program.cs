a67bbb4af8041a2e5d05e71251243865456212ee
Ondrej Rysavy <rysavy.ondrej@gmail.com>
2018-11-15 21:31:55 +0900

    Add sample project

---
+++ samples/TlsClassification/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TlsClassification
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
